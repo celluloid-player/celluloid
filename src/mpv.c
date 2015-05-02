07808ecf9193bc8e097bd3c0f960f6722b115e63
gnome-mpv <gnome-mpv@nowhere>
2015-05-01 23:02:04 -0500

    mpv: Set the option 'title' to value of the property 'media-title'

--- src/mpv.c
+++ src/mpv.c
@@ -573,6 +573,10 @@ void mpv_init(gmpv_handle *ctx, gint64 vid_area_wid)
 						ICON_NAME ));
 
 	mpv_check_error(mpv_set_option_string(	ctx->mpv_ctx,
+						"title",
+						"${media-title}" ));
+
+	mpv_check_error(mpv_set_option_string(	ctx->mpv_ctx,
 						"input-cursor",
 						"no" ));
 
