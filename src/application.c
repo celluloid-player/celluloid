29e0063bef96071b82ddda557ad77484bf36ce06
gnome-mpv <gnome-mpv@nowhere>
2016-03-09 10:47:43 +0700

    application: Fix fullscreen button showing up when CSD is enabled

--- src/application.c
+++ src/application.c
@@ -218,6 +218,11 @@ static void startup_handler(GApplication *gapp, gpointer data)
 			(GTK_APPLICATION(app), G_MENU_MODEL(full_menu));
 	}
 
+	setup_accelerators(app);
+	actionctl_map_actions(app);
+	main_window_load_state(app->gui);
+	gtk_widget_show_all(GTK_WIDGET(app->gui));
+
 	if(csd_enable)
 	{
 		control_box_set_fullscreen_btn_visible
@@ -226,11 +231,6 @@ static void startup_handler(GApplication *gapp, gpointer data)
 
 	control_box_set_chapter_enabled
 		(CONTROL_BOX(app->gui->control_box), FALSE);
-
-	setup_accelerators(app);
-	actionctl_map_actions(app);
-	main_window_load_state(app->gui);
-	gtk_widget_show_all(GTK_WIDGET(app->gui));
 
 	/* Due to a GTK bug, get_xid() must not be called when opengl-cb is
 	 * enabled or the GtkGLArea will break.
