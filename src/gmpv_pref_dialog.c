4c4102316e1eb11ef798cd6cfb4a0b879fd1b857
gnome-mpv <gnome-mpv@nowhere>
2016-05-30 01:58:29 +0700

    pref_dialog: Correctly set margin of content area when CSD is disabled
    gtk_widget_set_margin_bottom() was being called on dlg->grid, which is
    no longer used. Fix this by calling the function on the content area of
    the dialog instead.
    
    Fixes #184.

--- src/gmpv_pref_dialog.c
+++ src/gmpv_pref_dialog.c
@@ -32,7 +32,6 @@ struct _GmpvPrefDialog
 	GtkDialog parent_instance;
 	GSettings *settings;
 	GtkWidget *notebook;
-	GtkWidget *grid;
 };
 
 struct _GmpvPrefDialogClass
@@ -254,7 +253,11 @@ static void pref_dialog_constructed(GObject *obj)
 
 	if(!csd_enabled)
 	{
-		gtk_widget_set_margin_bottom(GMPV_PREF_DIALOG(obj)->grid, 12);
+		GtkWidget *content_area =	gtk_dialog_get_content_area
+						(GTK_DIALOG(obj));
+
+		gtk_widget_set_margin_bottom(content_area, 12);
+		gtk_widget_set_margin_bottom(GMPV_PREF_DIALOG(obj)->notebook, 6);
 	}
 
 	G_OBJECT_CLASS(gmpv_pref_dialog_parent_class)->constructed(obj);
