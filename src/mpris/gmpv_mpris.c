f5c0948b176270cfa2b6be5d6c254edeeb179944
gnome-mpv <gnome-mpv@nowhere>
2018-10-27 14:32:58 +0700

    mpris: Create separate DBus connection for each window
    The instance ID is also updated to just be the window ID from
    GtkApplicationWindow.
    
    Fixes #388.

--- src/mpris/gmpv_mpris.c
+++ src/mpris/gmpv_mpris.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2017 gnome-mpv
+ * Copyright (c) 2015-2018 gnome-mpv
  *
  * This file is part of GNOME MPV.
  *
@@ -73,56 +73,40 @@ static void name_lost_handler(	GDBusConnection *connection,
 				gpointer data );
 static void unregister(GmpvMpris *mpris);
 
-static const gchar *get_instance_id(void)
-{
-	static gchar *instance_id = NULL;
-
-	if (g_once_init_enter(&instance_id))
-	{
-		g_autoptr(GKeyFile) keyfile = g_key_file_new();
-		gchar *id = NULL;
-
-		/* In a PID namespace (flatpak) you cannot rely on
-		 * a PID being a unique identifier so lets just use
-		 * the one assigned by flatpak itself.
-		 */
-		if (g_key_file_load_from_file(	keyfile,
-						"/.flatpak-info",
-						G_KEY_FILE_NONE,
-						NULL	))
-		{
-			id = g_key_file_get_string(	keyfile,
-							"Instance",
-							"instance-id",
-							NULL	);
-		}
-
-		if (!id)
-		{
-			/* sizeof(pid_t) can theoretically be larger than sizeof(gint64), but
-			 * even then the chance of collision would be minimal.
-			 */
-			id = g_strdup_printf("%"G_GINT64_FORMAT, ABS((gint64)getpid()));
-		}
-
-		g_debug("Got instance-id: %s", id);
-		g_once_init_leave(&instance_id, id);
-	}
-
-	return instance_id;
-}
-
 static void constructed(GObject *object)
 {
 	GmpvMpris *self = GMPV_MPRIS(object);
-	gchar *name = NULL;
+	gchar *address = NULL;
+	GDBusConnection *conn =	NULL;
 
-	name =	g_strdup_printf(MPRIS_BUS_NAME ".instance-%s", get_instance_id());
+	address =	g_dbus_address_get_for_bus_sync
+			(G_BUS_TYPE_SESSION, NULL, NULL);
 
-	self->name_id = g_bus_own_name(	G_BUS_TYPE_SESSION,
+	if(address)
+	{
+		const GDBusConnectionFlags flags =
+			G_DBUS_CONNECTION_FLAGS_AUTHENTICATION_CLIENT|
+			G_DBUS_CONNECTION_FLAGS_MESSAGE_BUS_CONNECTION;
+
+		conn =	g_dbus_connection_new_for_address_sync
+			(address, flags, NULL, NULL, NULL);
+
+		g_free(address);
+	}
+
+	if(conn)
+	{
+		GmpvView *view = gmpv_controller_get_view(self->controller);
+		GmpvMainWindow *window = gmpv_view_get_main_window(view);
+		guint window_id =	gtk_application_window_get_id
+					(GTK_APPLICATION_WINDOW(window));
+		gchar *name =	g_strdup_printf
+				(MPRIS_BUS_NAME ".instance-%u", window_id);
+
+		self->name_id =	g_bus_own_name_on_connection
+				(	conn,
 					name,
 					G_BUS_NAME_OWNER_FLAGS_NONE,
-					NULL,
 					(GBusNameAcquiredCallback)
 					name_acquired_handler,
 					(GBusNameLostCallback)
@@ -130,7 +114,13 @@ static void constructed(GObject *object)
 					self,
 					NULL );
 
-	g_free(name);
+		g_free(name);
+	}
+	else
+	{
+		g_warning(	"Failed to create DBus connection; "
+				"MPRIS will be unavailable" );
+	}
 
 	G_OBJECT_CLASS(gmpv_mpris_parent_class)->constructed(object);
 }
