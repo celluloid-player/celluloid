bf1cef484e4ed5fbf521c2bdc6d84b77bcc03748
gnome-mpv <gnome-mpv@nowhere>
2019-11-21 21:02:40 +0700

    Retain window maximization state across sessions
    Fixes #489.

--- src/celluloid-main-window.c
+++ src/celluloid-main-window.c
@@ -501,12 +501,14 @@ celluloid_main_window_save_state(CelluloidMainWindow *wnd)
 	CelluloidMainWindowPrivate *priv;
 	gint width;
 	gint height;
+	gboolean maximized;
 	gint handle_pos;
 	gdouble volume;
 	gboolean controls_visible;
 
 	settings = g_settings_new(CONFIG_WIN_STATE);
 	priv = get_private(wnd);
+	maximized = gtk_window_is_maximized(GTK_WINDOW(wnd));
 	handle_pos = gtk_paned_get_position(GTK_PANED(priv->vid_area_paned));
 	volume =	celluloid_control_box_get_volume
 			(CELLULOID_CONTROL_BOX(priv->control_box));
@@ -516,6 +518,7 @@ celluloid_main_window_save_state(CelluloidMainWindow *wnd)
 
 	g_settings_set_int(settings, "width", width);
 	g_settings_set_int(settings, "height", height);
+	g_settings_set_boolean(settings, "maximized", maximized);
 	g_settings_set_double(settings, "volume", volume);
 	g_settings_set_boolean(settings, "show-controls", controls_visible);
 	g_settings_set_boolean(settings, "show-playlist", priv->playlist_visible);
@@ -545,6 +548,7 @@ celluloid_main_window_load_state(CelluloidMainWindow *wnd)
 		CelluloidMainWindowPrivate *priv = get_private(wnd);
 		gint width = g_settings_get_int(settings, "width");
 		gint height = g_settings_get_int(settings, "height");
+		gboolean maximized = g_settings_get_boolean(settings, "maximized");
 		gint handle_pos;
 		gboolean controls_visible;
 		gdouble volume;
@@ -565,6 +569,11 @@ celluloid_main_window_load_state(CelluloidMainWindow *wnd)
 		gtk_window_resize(GTK_WINDOW(wnd), width, height);
 		gtk_paned_set_position
 			(GTK_PANED(priv->vid_area_paned), handle_pos);
+
+		if(maximized)
+		{
+			gtk_window_maximize(GTK_WINDOW(wnd));
+		}
 
 		g_clear_object(&settings);
 	}
