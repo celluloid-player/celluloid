f215c8968ebd6a124f0c4d00556bf3de4c1e3258
gnome-mpv <gnome-mpv@nowhere>
2020-07-06 13:07:30 +0700

    view: Update copyright year in about dialog

--- src/celluloid-view.c
+++ src/celluloid-view.c
@@ -1548,7 +1548,7 @@ celluloid_view_show_about_dialog(CelluloidView *view)
 				"license-type",
 				GTK_LICENSE_GPL_3_0,
 				"copyright",
-				"\u00A9 2014-2019 The Celluloid authors",
+				"\u00A9 2014-2020 The Celluloid authors",
 				"authors",
 				authors,
 				"translator-credits",
