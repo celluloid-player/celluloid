d16c8326e5bc2627b2cbf0c0f59e15399a05867b
gnome-mpv <gnome-mpv@nowhere>
2020-07-13 01:34:29 +0700

    preferences-dialog: Set CSS name

--- src/celluloid-preferences-dialog.c
+++ src/celluloid-preferences-dialog.c
@@ -293,6 +293,8 @@ celluloid_preferences_dialog_class_init(CelluloidPreferencesDialogClass *klass)
 	wid_class->key_press_event = key_press_handler;
 	GTK_DIALOG_CLASS(klass)->response = response_handler;
 	G_OBJECT_CLASS(klass)->constructed = preferences_dialog_constructed;
+
+	gtk_widget_class_set_css_name(wid_class, "celluloid-preferences-dialog");
 }
 
 static void
