bbb4d4f06a5a0e4b9a43b9af2e94b5fa0801d395
Bobby Rong <rjl931189261@126.com>
2022-01-04 18:52:28 +0800

    meson-post-install.py: use gtk4-update-icon-cache
    Current script fails if gtk3 is not installed.

--- meson-post-install.py
+++ meson-post-install.py
@@ -12,7 +12,7 @@ if not 'DESTDIR' in os.environ:
 	subprocess.call(['glib-compile-schemas', os.path.join(datadir, 'glib-2.0', 'schemas')])
 
 	print('Updating icon cache...')
-	subprocess.call(['gtk-update-icon-cache', '-qtf', os.path.join(datadir, 'icons', 'hicolor')])
+	subprocess.call(['gtk4-update-icon-cache', '-qtf', os.path.join(datadir, 'icons', 'hicolor')])
 
 	print('Updating desktop database...')
 	subprocess.call(['update-desktop-database', '-q', os.path.join(datadir, 'applications')])
