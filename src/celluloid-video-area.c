7f4b86320b7b4aa67ccf1de3dba73d20be15b4a7
gnome-mpv <gnome-mpv@nowhere>
2022-07-08 20:26:43 +0700

    video-area: Set size request for control box's revealer

--- src/celluloid-video-area.c
+++ src/celluloid-video-area.c
@@ -569,12 +569,25 @@ celluloid_video_area_init(CelluloidVideoArea *area)
 	gtk_revealer_set_child(	GTK_REVEALER(area->control_box_revealer),
 				area->control_box );
 
+	// Explicitly set size request for the control box revealer using the
+	// underlying control box's size. This is needed because the revealer's
+	// size request will be zero otherwise.
+	GtkRequisition minimum = {};
+
+	gtk_widget_get_preferred_size
+		(area->control_box, &minimum, NULL);
+	gtk_widget_set_size_request
+		(area->control_box_revealer, minimum.width, minimum.height);
+
 	gtk_overlay_add_overlay
 		(GTK_OVERLAY(area->overlay), area->control_box_revealer);
 	gtk_overlay_add_overlay
 		(GTK_OVERLAY(area->overlay), area->header_bar_revealer);
+	gtk_overlay_set_measure_overlay
+		(GTK_OVERLAY(area->overlay), area->control_box_revealer, TRUE);
+	gtk_overlay_set_child
+		(GTK_OVERLAY(area->overlay), area->stack);
 
-	gtk_overlay_set_child(GTK_OVERLAY(area->overlay), area->stack);
 	gtk_box_append(GTK_BOX(area), area->overlay);
 
 	celluloid_control_box_set_floating
