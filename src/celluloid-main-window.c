5e771b0e2d665d377dbd0042ce2a696384b43a8a
gnome-mpv <gnome-mpv@nowhere>
2022-07-08 20:01:48 +0700

    main-window: Make video area non-shrinkable

--- src/celluloid-main-window.c
+++ src/celluloid-main-window.c
@@ -125,6 +125,8 @@ constructed(GObject *object)
 		(GTK_PANED(priv->video_area_paned), priv->video_area);
 	gtk_paned_set_end_child
 		(GTK_PANED(priv->video_area_paned), priv->playlist);
+	gtk_paned_set_shrink_start_child
+		(GTK_PANED(priv->video_area_paned), FALSE);
 	gtk_paned_set_shrink_end_child
 		(GTK_PANED(priv->video_area_paned), FALSE);
 
