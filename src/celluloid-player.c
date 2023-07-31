9b83837e05aa01a8eecf416d1f85fde313f208ba
gnome-mpv <gnome-mpv@nowhere>
2023-07-31 14:47:21 +0700

    player: Remove vo from the list of default options

--- src/celluloid-player.c
+++ src/celluloid-player.c
@@ -512,8 +512,7 @@ apply_default_options(CelluloidMpv *mpv)
 		const gchar *name;
 		const gchar *value;
 	}
-	options[] = {	{"vo", "opengl,vdpau,vaapi,xv,x11,libmpv,"},
-			{"osd-level", "1"},
+	options[] = {	{"osd-level", "1"},
 			{"softvol", "yes"},
 			{"force-window", "immediate"},
 			{"input-default-bindings", "yes"},
